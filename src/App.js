540727c419d0c673319509db680955281ce1ee71
Angel Muelas <angel.muelas@gmail.com>
2017-07-06 20:17:02 +0200

    added new file for creating a new contact and element into app file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 import * as ContactsAPI from './utils/ContactsAPI';
+import CreateContact from './CreateContact'
 
 class App extends Component {
   state = {
-    contacts: []
+    contacts: [],
+    screen: 'create' //list, create
   }
 
   componentDidMount() {
@@ -22,10 +24,15 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <ListContacts
-          onDeleteContact={this.removeContact}
-          contacts={this.state.contacts} />
+      <div className='app'>
+        {this.state.screen == 'list' && (
+          <ListContacts
+            onDeleteContact={this.removeContact}
+            contacts={this.state.contacts} />
+        )}
+        {this.state.screen == 'create' && (
+          <CreateContact/>
+        )}
       </div>
     )
   }
