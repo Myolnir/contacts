be002442850a41eb29026b941552eaa9dab77c90
Angel Muelas <angel.muelas@gmail.com>
2017-07-09 15:55:38 +0200

    insert route tag instead ifs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts';
 import * as ContactsAPI from './utils/ContactsAPI';
 import CreateContact from './CreateContact'
 
 class App extends Component {
   state = {
-    contacts: [],
-    screen: 'list' //list, create
+    contacts: []
   }
 
   componentDidMount() {
@@ -25,18 +25,13 @@ class App extends Component {
   render() {
     return (
       <div className='app'>
-        {this.state.screen == 'list' && (
+        <Route exact path="/" render={() => (
           <ListContacts
             onDeleteContact={this.removeContact}
             contacts={this.state.contacts}
-            onNavigate={() => {
-              this.setState({screen : 'create'})
-            }}
            />
-        )}
-        {this.state.screen == 'create' && (
-          <CreateContact/>
-        )}
+        )}/>
+        <Route path='/create' component={CreateContact}/>
       </div>
     )
   }
